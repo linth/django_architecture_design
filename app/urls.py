b3dd21a49671890fcb8b77adf2075d81489604c0
linth <zhua0404@gmail.com>
2020-04-25 16:00:57 +0800

    add a index page from django application.

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from app import views
+
+app_name = 'app'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app/', include('app.urls', namespace='app')),
+    path('', views.index, name='index'),
 ]
